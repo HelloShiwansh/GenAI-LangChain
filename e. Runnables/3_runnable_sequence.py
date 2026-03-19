e760ae9b34b1ec0a1b0fecde8a3317725421f7ee
HelloShiwansh <hellosms2005@gmail.com>
2026-03-20 02:13:54 +0530

    Continuing Runnables

---
+++ e. Runnables/3_runnable_sequence.py
@@ -0,0 +1,26 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+
+load_dotenv()
+
+model = ChatGroq(model="llama-3.1-8b-instant", temperature=1.9)
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+
+print(chain.invoke({"topic": "war"}))
---
+++ e. Runnables/4_runnable_parallel.py
@@ -0,0 +1,31 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence, RunnableParallel
+
+load_dotenv()
+
+model = ChatGroq(model="llama-3.1-8b-instant", temperature=1.9)
+
+prompt1 = PromptTemplate(
+    template='Generate a tweet about {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a Linkedin post about {topic}',
+    input_variables=['topic']
+)
+
+parser = StrOutputParser()
+
+parallel_chain = RunnableParallel({
+    'tweet': RunnableSequence(prompt1, model, parser),
+    'linkedin': RunnableSequence(prompt2, model, parser)
+})
+
+result = parallel_chain.invoke({'topic':'AI'})
+
+print(result['tweet'])
+print(result['linkedin'])
